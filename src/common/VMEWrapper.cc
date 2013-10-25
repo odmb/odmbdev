115cbddbe776c479cf5878cd06fa060a650c10de
Jack Bradmiller-Feld <jbradmil@cern.ch>
2013-10-25 14:55:33 -0700

    Improved log formatting.

--- src/common/VMEWrapper.cc
+++ src/common/VMEWrapper.cc
@@ -26,9 +26,19 @@ namespace emu { namespace odmbdev {
     	int addr = (address & 0x07ffff) | shiftedSlot;
     	char rcv[2];
         crate_->vmeController()->vme_controller(3,addr,&command,rcv);
-        logger_ << "W  " << std::setw(4) << hex << address << std::setw(6) << hex << command
-        		<< "  S" << std::setw(2) << dec << slot 
-        		<< "    " << comment.c_str() << endl;
+        // Get timestamp
+        time_t rawtime;
+  		struct tm * timeinfo;
+ 		time ( &rawtime );
+  		timeinfo = localtime ( &rawtime );
+  		char timestamp[30];
+  		strftime (timestamp,30,"%D %X",timeinfo);
+  		// Write to log
+        logger_ << "W" << std::setw(7) << hex << address
+        		<< std::setw(7) << hex << command
+        		<< std::setw(15) << dec << slot 
+        		<< "   " << timestamp
+        	    << "    " << comment.c_str() << endl;
     }
     unsigned short int VMEWrapper::VMERead (unsigned short int address, unsigned int slot, string comment) {
     	unsigned int shiftedSlot = slot << 19;
@@ -37,8 +47,28 @@ namespace emu { namespace odmbdev {
         char rcv[2];
         crate_->vmeController()->vme_controller(2,addr,&data,rcv);
         unsigned short int result = (rcv[1] & 0xff) * 0x100 + (rcv[0] & 0xff);
-        logger_ << "R  " << std::setw(4) << hex << address << std::setw(6) << "0"
-        	    << "  S" << std::setw(2) << dec<< slot << std::setw(16) << hex << result 
+        
+        // Get timestamp
+        time_t rawtime;
+  		struct tm * timeinfo;
+ 		time ( &rawtime );
+  		timeinfo = localtime ( &rawtime );
+  		char timestamp[30];
+  		strftime (timestamp,30,"%D %X",timeinfo);
+        // Get formatting
+        bool readHex = true;
+        if((address >= 0x321C && address <= 0x337C) || (address >= 0x33FC && address < 0x35AC)  || 
+	       (address > 0x35DC && address <= 0x3FFF) || address == 0x500C || address == 0x510C ||
+	       address == 0x520C || address == 0x530C || address == 0x540C   || address == 0x550C || address == 0x560C 
+           || address == 0x8004 ||  (address == 0x5000 && result < 0x1000)) {
+	      		readHex = false;
+	      }
+	    // Write to log
+        logger_ << "R" << std::setw(7) << hex << address;
+        if (readHex) logger_<< std::setw(14) << hex << result;
+        else logger_<< std::setw(12) << dec << result << "_d";
+        logger_	<< std::setw(8) << dec << slot 
+        	    << "   " << timestamp
         	    << "    " << comment.c_str() << endl;
         return result;
     }
