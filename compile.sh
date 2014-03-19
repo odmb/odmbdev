c637502a2ad0899df0c5826c1b30c7de9d839dd4
Adam Dishaw <aldishaw@gmail.com>
2014-03-18 20:21:34 -0700

    Allow compilation to proceed with warnings

--- compile.sh
+++ compile.sh
@@ -14,29 +14,28 @@ export XDAQ_ROOT=/opt/xdaq
 export XDAQ_OS=linux
 export XDAQ_PLATFORM=x86_64_slc5
 
-tmp_file="makefile_errors_and_warnings_temporary_collision_free_long_unambiguous_name.log"
+tmp_file_build="makefile_errors_and_warnings_temporary_collision_free_long_unambiguous_name_build.log"
+tmp_file_install="makefile_errors_and_warnings_temporary_collision_free_long_unambiguous_name_install.log"
 
-make -j -k 2> >(tee $tmp_file >&2)
+make -j -k 2> >(tee $tmp_file_build >&2)
+make -j -k install 2> >(tee $tmp_file_install >&2)
 
 echo
 
-if [[ -s $tmp_file ]] ; then
+if [[ -s $tmp_file_build || -s $tmp_file_install ]] ; then
     echo
     echo "ERRORS AND WARNINGS:"
-    cat $tmp_file >&2
-    rm -rf $tmp_file
+    if [[ -s $tmp_file_build ]] ; then
+	cat $tmp_file_build >&2
+	rm -rf $tmp_file_build
+    fi
+    if [[ -s $tmp_file_install ]] ; then
+	cat $tmp_file_install >&2
+	rm -rf $tmp_file_install
+    fi
     exit 1
 else
-    make -j -k install 2> >(tee $tmp_file >&2)
-    if [[ -s $tmp_file ]] ; then
-	echo
-	echo "ERRORS AND WARNING:"
-	cat $tmp_file >&2
-	rm -rf $tmp_file
-	exit 2
-    else
-	echo
-	echo "Compiled successfully without errors or warnings!"
-	exit 0
-    fi
+    echo
+    echo "Compiled successfully without errors or warnings!"
+    exit 0
 fi
